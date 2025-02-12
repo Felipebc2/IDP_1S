77f8a167d51a0b64e58d6d6b471aa56317e7e8cf
Felipe B. Castro <felipeb.castro02@gmail.com>
2025-02-12 17:33:35 -0300

    Add files via upload

---
+++ Algoritmos/Aula 1 - Hello World/helloworld.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+int main () {
+    printf("Hello world!\n");
+    int idade1 = 20;
+    printf("a idade um é %i\n", idade1);
+    int idade2 = 21;
+    printf("a idade dois é %i\n", idade2);
+    char inicialNome = 'L';
+    printf("Minha inicial é %c\n", inicialNome);
+    int tamanhoDoInt = sizeof(int);
+    printf("o int tem %i byte(s)\n", tamanhoDoInt);
+    char Hello = 'H';
+    printf("%c", Hello);
+    return 0;
+}
+
