bfbb1514042a08b5669169918af98ece2d6452f3
Felipe B. Castro <felipeb.castro02@gmail.com>
2025-02-12 17:34:18 -0300

    Add files via upload

---
+++ Algoritmos/Aula 1 - Hello World/Aula 2 - Bytes/Bytes.c
@@ -0,0 +1,16 @@
+// Nome: Felipe Barroso de Castro
+// RA: 2311292
+// Data: 31/08/2023
+#include "stdio.h"
+
+int main() 
+{
+    printf("Tamanho de char: %lu bytes\n", sizeof(char));
+    printf("Tamanho de short: %lu bytes\n", sizeof(short));
+    printf("Tamanho de int: %lu bytes\n", sizeof(int));
+    printf("Tamanho de float: %lu bytes\n", sizeof(float));
+    printf("Tamanho de double: %lu bytes\n", sizeof(double));
+    printf("Tamanho de long: %lu bytes\n", sizeof(long));
+
+return 0;
+}
